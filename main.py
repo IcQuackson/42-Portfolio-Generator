496ba20eb864580cb4217b5b9c1e6b817b489380
Pedro Gonçalves <61185097+IcQuackson@users.noreply.github.com>
2023-04-01 01:01:46 +0100

    it now outputs pdf using pdfkit
    Instalando as libs todas ja dá para usar o pdfkit mas por alguma razao nao consigo pôr as informacoes no pdf

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
 from oauthlib.oauth2 import BackendApplicationClient
 from requests_oauthlib import OAuth2Session
 import requests
-import os
+import pdfkit
+import portfolio
+import json
 
-UID = os.environ['42UID']
-SECRET = os.environ['42SECRET']
+UID = "u-s4t2ud-a3460dc27a5dc4b8d1fbba961bfab6be66dc7eb02886000ce33e2b4c7976a0f6"
+SECRET = "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"
 
 # Create a client object with your credentials
 client = BackendApplicationClient(client_id=UID)
@@ -28,8 +30,28 @@ def get_student_data(username):
 def get_completed_projects(username):
     headers = {"Authorization": f"Bearer {ACCESS_TOKEN}"}
     response = requests.get(f"https://api.intra.42.fr/v2/users/{username}/projects_users?filter[status]=finished", headers=headers)
+  
     projects_data = response.json()
-    return projects_data
+    filtered_projects_data = [project for project in projects_data if project['validated?'] == True and "Piscine" not in project['project']['name']]
+    return filtered_projects_data
+
+# Define function to generate HTML from data
+def generate_html(student_data, completed_projects):
+    projects_html = ""
+    for project in completed_projects:
+      project_name = project['project']['name']
+      final_mark = project['final_mark']
+      projects_html += f"<tr><td class='project-name'>{project_name}</td><td class='final-mark'>{final_mark}</td></tr>"
+    html = portfolio.html_template.format(name=student_data['displayname'], email=student_data['email'], phone=student_data['phone'], projects=projects_html)
+    return html
+
+def get_project_details(project_id):
+    headers = {"Authorization": f"Bearer {ACCESS_TOKEN}"}
+    response = requests.get(f"https://api.intra.42.fr/v2/projects/{project_id}", headers=headers)
+    project_data = response.json()
+    with open("output.txt", "w") as f:
+      f.write(json.dumps(project_data, indent=4))
+    return project_data
 
 def print_projects(student_data, completed_projects):
   # Output student information and completed projects
@@ -38,8 +60,8 @@ def print_projects(student_data, completed_projects):
   print(f"Phone: {student_data['phone']}")
   print(f"Completed projects:")
   for project in completed_projects:
-    if project['validated?'] == True and "Piscine" not in project['project']['name']:
-      print(f"\t{project['project']['name']} - final mark: {project['final_mark']}")
+    print(f"\t{project['project']['name']} - final mark: {project['final_mark']}")
+    #project_details = get_project_details(project['project']['id'])
   
 # Get input username from user
 username = ""
@@ -47,9 +69,12 @@ while username != "exit":
   username = input("Enter student username: ")
   # Retrieve student data and completed projects using the 42 API
   student_data = get_student_data(username)
-  completed_projects = get_completed_projects(username)
   if student_data is None:
         print(f"Error: User {username} not found.\n")
         continue
+  completed_projects = get_completed_projects(username)
   print_projects(student_data, completed_projects)
+  html = generate_html(student_data, completed_projects)
+  # Generate PDF from HTML using pdfkit
+  pdfkit.from_string(portfolio.html_template, f"{username}_projects.pdf")
   print()
